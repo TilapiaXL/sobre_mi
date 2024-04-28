22f958443dd5b93cbc2b451410c2f7b30ee6ed2e
TilapiaXL <168359795+TilapiaXL@users.noreply.github.com>
2024-04-28 11:28:48 -0500

    Create mi_informacion.py
    Aqui puedes ver algunas cosas sobre mi :)

---
+++ mi_informacion.py
@@ -0,0 +1,3 @@
+import random
+sobre_mi = ["Hola, soy Gabriel Tapia", "Mi tema favorita es la astronomía", "Tengo un pug como mascota", "vivo en Ecuador", "estoy aprendiendo a tocar la guitarra", "mi deporte favorito es la natación", "tengo 14 años", "yo como lo que sea que me sirvan en la mesa menos carne en la sopa"]
+print(random.choice(sobre_mi))
